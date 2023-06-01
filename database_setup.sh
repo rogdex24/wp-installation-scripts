d45b95eb87673423d2169151da9eae571a0c5620
Abu Talha <itsabutalha@gmail.com>
2023-06-01 18:01:14 +0530

    secure string fix

--- database_setup.sh
+++ database_setup.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y mysql-server
-service mysql start
+#!/bin/basha
 
 # Retrieve the parameter values
 DB_HOST=$(aws ssm get-parameter --name "/myapp/DB_HOST" --query "Parameter.Value" --output text)
-DB_USER=$(aws ssm get-parameter --name "/myapp/DB_USER" --query "Parameter.Value" --output text)
-DB_PASSWORD=$(aws ssm get-parameter --name "/myapp/DB_PASSWORD" --query "Parameter.Value" --output text)
+DB_USER=$(aws ssm get-parameter --name "/myapp/DB_USER" --with-decryption --query "Parameter.Value" --output text)
+DB_PASSWORD=$(aws ssm get-parameter --name "/myapp/DB_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
 DB_NAME=$(aws ssm get-parameter --name "/myapp/DB_NAME" --query "Parameter.Value" --output text)
-DB_NEWUSER=$(aws ssm get-parameter --name "/myapp/DB_NEWUSER" --query "Parameter.Value" --output text)
-DB_NEWPASSWORD=$(aws ssm get-parameter --name "/myapp/DB_NEWPASSWORD" --query "Parameter.Value" --output text)
+DB_NEWUSER=$(aws ssm get-parameter --name "/myapp/DB_NEWUSER" --with-decryption --query "Parameter.Value" --output text)
+DB_NEWPASSWORD=$(aws ssm get-parameter --name "/myapp/DB_NEWPASSWORD" --with-decryption --query "Parameter.Value" --output text)
+
 
 # MySQL setup script
 mysql -h "${DB_HOST}" -u "${DB_USER}" -p"${DB_PASSWORD}" <<EOF
